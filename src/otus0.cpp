9f2864fa457b8e797e157f2d40bc884a811e840e
Dmitry Mukhoedov <muhadov@relex.ru>
2017-12-05 11:46:30 +0300

    home work with versioning

---
+++ src/otus0.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "include/otus0.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello world!" << " " << _VERSION_MAJOR << "." << _VERSION_MINOR << "." << _VERSION_PATCH << std::endl;
+    return 0;
+}
