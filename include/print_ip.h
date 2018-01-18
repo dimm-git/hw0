5c898c5e3550315e6280fbef75d8d0b3a1ae0eff
dimm <Dmitry.Mukhoedov@gmail.com>
2018-01-18 23:30:11 +0300

    improve stream output

--- include/print_ip.h
+++ include/print_ip.h
@@ -12,31 +12,36 @@
 
 //! @brief Функция вывода в поток целых чисел
 template <typename T>
-typename std::enable_if<std::is_integral<T>::value, void>::type print_ip(std::ostream& strm, const T& val)
+typename std::enable_if<std::is_integral<T>::value, std::ostream&>::type print_ip(std::ostream& strm, const T& val)
 {
-    impl::print_ord<T>::print(strm, val);
+    impl::print_ord<T, sizeof(T)>::print(strm, val);
+    return strm;
 }
 
 //! @brief Функция вывода в поток полей кортежей.
 //! @warning Поля кортежа должны быть одного типа
 //! @warning В отличии от версии для целых чисел, нет специализаии для char и signed типов
 template <class... Args>
-auto print_ip(std::ostream& strm, const std::tuple<Args...>& val) -> typename std::enable_if<impl::check_tuple<std::remove_reference_t<decltype(val)> >::value, void>::type
+auto print_ip(std::ostream& strm, const std::tuple<Args...>& val) -> typename std::enable_if<impl::check_tuple<std::remove_reference_t<decltype(val)> >::value, std::ostream&>::type
 {
     typedef typename std::remove_reference<decltype(val)>::type U;
     impl::print_tuple<U, std::tuple_size<U>::value>::print(strm, val);
+    return strm;
 }
 
 //! @brief Функция вывода в поток контейнера (класса с константным итератором).
 //! @warning В отличии от версии для целых чисел, нет специализаии для char и signed типов
 template <typename T>
-typename std::enable_if<impl::is_container<T>::value, void>::type print_ip(std::ostream& strm, const T& val)
+typename std::enable_if<impl::is_container<T>::value, std::ostream&>::type print_ip(std::ostream& strm, const T& val)
 {
     impl::print_container<T>::print(strm, val);
+    return strm;
 }
 
+//! @brief Функция вывода в поток ip-адреса из строки.
 template <>
-void print_ip<std::string>(std::ostream& strm, const std::string& str)
+std::ostream& print_ip<std::string>(std::ostream& strm, const std::string& str)
 {
     strm << str;
+    return strm;
 }
--- src/ip_printer.cpp
+++ src/ip_printer.cpp
@@ -11,27 +11,17 @@ int main(int, char**)
 {
     try
     {
-        print_ip(std::cout, char(-1));
-        std::cout << std::endl;
-        print_ip(std::cout, short(0));
-        std::cout << std::endl;
-        print_ip(std::cout, int(2130706433));
-        std::cout << std::endl;
-        print_ip(std::cout, long(8875824491850138409));
-        std::cout << std::endl;
-        //!!!!!
-        print_ip(std::cout, std::string("ip.from.string"));
-        std::cout << std::endl;
+        print_ip(std::cout, char(-1)) << std::endl;
+        print_ip(std::cout, short(0)) << std::endl;
+        print_ip(std::cout, int(2130706433)) << std::endl;
+        print_ip(std::cout, long(8875824491850138409)) << std::endl;
+        print_ip(std::cout, std::string("ip.from.string")) << std::endl;
         std::vector<int> v = { 1, 2, 3, 4, 5 };
-        print_ip(std::cout, v);
-        std::cout << std::endl;
+        print_ip(std::cout, v) << std::endl;
 
         std::list<int> l = { 6, 7, 8, 9, 10 };
-        print_ip(std::cout, l);
-        std::cout << std::endl;
-
-        print_ip(std::cout, std::make_tuple(11, 12, 13, 14, 15, 16));
-        std::cout << std::endl;
+        print_ip(std::cout, l) << std::endl;
+        print_ip(std::cout, std::make_tuple(11, 12, 13, 14, 15, 16)) << std::endl;
     }
     catch(const std::exception& e)
     {
