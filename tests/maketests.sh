4dac4bcc597edbe556a8f14f24c594422db83684
Dmitry Mukhoedov <Dmitry.Mukhoedov@gmail.com>
2018-02-19 01:10:44 +0300

    add more tests

---
+++ tests/maketests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+D=`dirname "$0"`
+I=`realpath "$D"`
+
+for I in `seq 0 6`; do
+  echo $I
+  "$1" 3 < "$D/data/${I}_input.txt" > output.txt
+  diff -u "$D/data/${I}_output.txt" "output.txt" || {
+    echo "$I: diffs found"
+    exit 1
+  }
+done;
+
+echo "OK"
+
+exit 0
