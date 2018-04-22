1ae63a2cd8686354e81ec4f18cc1739ddbbb6db8
dimm <Dmitry.Mukhoedov@gmail.com>
2018-04-22 19:17:25 +0300

    visualization

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 OTUS C++ course  
 
-The "join_server" project  
+The "kkmeans" project  
 
+Usage:
+kgen 0 > data.txt - to generate data like kkmeans_ex.cpp
+kgen 1 > data.txt - to generate 'starry sky' data
+  
+cat kkmeans_ex.txt | kkmeans n - produce classified data output
+  
+Visualization:  
+2-field data:
+cat data.txt | kvis output.png
+  
+3-field (x,y,class) data:
+cat data.txt | kkmeans n | kvis output.png
+  
+  
+Debian package data:
+/usr/share/kkmeans/stars.txt - 'starry sky' data
+/usr/share/kkmeans/stars.png - 'starry sky' diagram
+/usr/share/kkmeans/stars_class.txt - 'starry sky' classified data
+/usr/share/kkmeans/stars_class.png - 'starry sky' classified diagram
+/usr/share/kkmeans/kkmeans_ex.txt - 'example-like' data
+/usr/share/kkmeans/kkmeans_ex.png - 'example-like' diagram
+/usr/share/kkmeans/kkmeans_ex_class.png - 'example-like' classified data diagram
--- src/kgen.cpp
+++ src/kgen.cpp
@@ -24,21 +24,51 @@
 
 #include "kfunc.h"
 
-int main()
+void gen_example(samples_type& s, dlib::rand& rnd);
+void gen_stars(samples_type& s, dlib::rand& rnd);
+
+int main(int argc, char* argv[])
 {
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << "(0|1)" << std::endl << "1 - as a kkmeans example" << std::endl << "2 - as a starry sky" << std::endl;
+        return 1;
+    }
     samples_type samples;
 
     dlib::rand rnd;
 
+    if (std::atoi(argv[1]) == 1)
+        gen_stars(samples, rnd);
+    else
+        gen_example(samples, rnd);
+
+    for (auto& s : samples)
+        std::cout << s(0) << ";" << s(1) << std::endl;
+
+    return 0;
+}
+
+void gen_example(samples_type& samples, dlib::rand& rnd)
+{
     // we will make 50 points from each class
     const long num = 50;
 
     generate(samples, rnd, num, 0.5);
     generate(samples, rnd, num, 10);
     generate(samples, rnd, num, 4.0, 25, 25);
+}
 
-    for (auto& s : samples)
-        std::cout << s(0) << ";" << s(1) << std::endl;
-
-    return 0;
+void gen_stars(samples_type& s, dlib::rand& rand)
+{
+    const long num = 200;
+    sample_type m;
+    const double width = 100;
+    
+    for (long i = 0; i < num; ++i)
+    {
+        m(0) = (2 * width) * rand.get_random_double() - width;
+        m(1) = (2 * width) * rand.get_random_double() - width;
+        s.push_back(m);
+    }
 }
